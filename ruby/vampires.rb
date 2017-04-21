8ad91a302719009d240228aaaa81276a59043641
Dan <dedinsanity@yahoo.com>
2017-04-21 16:34:08 -0500

    Add question prompts

---
+++ ruby/vampires.rb
@@ -0,0 +1,14 @@
+puts "What is your name?"
+name = gets.chomp
+
+puts "What is your age?"
+age = gets.chomp
+
+puts "What year were you born?"
+date = gets.chomp
+
+puts "Our company serves garlic bread. Would you like some?"
+bread_pref = gets.chomp
+
+puts "Would you like to enroll in the health insurance plan?"
+healthcare = gets.chomp
