18924f1f18743287785bd43ef3e0a612180cffab
Dan <dedinsanity@yahoo.com>
2017-04-24 04:43:07 -0500

    Add loop

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,28 +1,60 @@
-puts "What is your name?"
-name = gets.chomp
-
-puts "What is your age?"
-age = gets.chomp
-
-puts "What year were you born?"
-date = gets.chomp
-
-puts "Would you like some garlic bread?"
-bread_pref = gets.chomp
-
-puts "Would you like to enroll in the health insurance plan?"
-healthcare = gets.chomp
-
-
 =begin
 
-if name == "tot", "hot"
-	p "100%"
-elsif (!correct_age && !bread_pref) || (!correct_age && !healthcare)
-	p "probs"
-elsif (correct_age && bread_pref && healthcare)
-	p "yup"
-else (correct_age && bread_pref) || (correct_age && healthcare)
-	p "nope"
+if name == "Drake Cula" || name == "Tu Fang"
+	p "Definitely a vampire."
+elsif (!correct_age && !bread) || (!correct_age && !health)
+	p "Probably a vampire."
+elsif (correct_age && bread && health)
+	p "Almost certainly a vampire."
+elsif (correct_age && bread) || (correct_age && health)
+	p "Probably not a vampire."
+else
+	p "Results inconclusive."
+end
 
 =end
+
+puts "How many employees?"
+goal = gets.chomp.to_i
+counter = 0
+while counter < goal
+	puts "What is your name?"
+	name = gets.chomp.downcase
+	p name
+	puts "What is your age?"
+	age = gets.chomp.to_i
+	p age
+	puts "What year were you born?"
+	date = gets.chomp.to_i
+	p date
+	puts "Would you like some garlic bread? (y/n)"
+	bread = gets.chomp
+	if bread == "y"
+		bread = true
+	else
+		bread = false
+	end
+	p bread
+	puts "Enroll in the health insurance plan? (y/n)"
+	health = gets.chomp
+	if health == "y"
+		health = true
+	else 
+		health = false
+	end
+	p health
+	unless name == "drake cula" || name == "tu fang"
+		if ((2017 == age + date) && bread) || (2017 == age + date && health)
+			p "Probably not a vampire."
+		elsif ((2017 != age + date) && !bread) || ((2017 != age + date) && !health)
+			p "Probably a vampire."
+		elsif ((2017 != age + date) && !bread && !health)
+			p "Almost certainly a vampire"
+		else
+			p "Results inconclusive."
+		end
+	else
+		p "Definitely a vampire."
+	end
+	counter += 1
+end
