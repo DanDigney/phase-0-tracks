7b6102a43b84628849f15330fd43e5ea4adeeb57
Dan <dedinsanity@yahoo.com>
2017-04-30 13:51:34 -0500

    Finalize with calls @end

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -2,41 +2,45 @@ murica = {
 	california: {
 		population: 38_802_500,
 		cities: { 
-			los_angeles: 
-			san_diego:
-			san_jose:
+			los_angeles: 4_000_000,
+			san_diego: 1_300_000,
+			san_jose: 946_000
 		}
 	},
 	texas: {
-		population:
+		population: 27_000_000,
 		cities: {
-			houston:
-			san_antonio:
-			dallas:
+			houston: 2_300_000,
+			san_antonio: 1_500_000,
+			dallas: 1_300_000
 		}
 	},
 	florida: {
-		population:
+		population: 19_900_000,
 		cities: {
-			jacksonville:
-			miami:
-			tampa:
+			jacksonville: 843_000,
+			miami: 409_000,
+			tampa: 353_000
 		}
 	},
 	new_york: {
 		population: 19_750_000,
 		cities: {
-			new_york_city: 8_500_00,
-			hempstead: 700_800,
-			brookhaven: 500_000
+			new_york_city: 8_500_000,
+			buffalo: 261_000,
+			rochester: 211_000
 		}
 	},
 	illinois: {
-		population:
+		population: 13_000_000,
 		cities: {
-			chicago:
-			aurora:
-			rockford:
+			chicago: 2_700_000,
+			aurora: 200_000,
+			rockford: 153_000
 		}
 	}
 }
+
+p murica[:new_york][:cities][:new_york_city]
+p murica[:texas][:population]
+p murica[:illinois]
