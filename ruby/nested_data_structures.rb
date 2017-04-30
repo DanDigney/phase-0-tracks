49566b80e942d91b377d5464fbc47d7d861e57ea
Dan <dedinsanity@yahoo.com>
2017-04-30 13:37:35 -0500

    Initial commit, basic structure

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,42 @@
+murica = {
+	california: {
+		population: 38_802_500,
+		cities: { 
+			los_angeles: 
+			san_diego:
+			san_jose:
+		}
+	},
+	texas: {
+		population:
+		cities: {
+			houston:
+			san_antonio:
+			dallas:
+		}
+	},
+	florida: {
+		population:
+		cities: {
+			jacksonville:
+			miami:
+			tampa:
+		}
+	},
+	new_york: {
+		population: 19_750_000,
+		cities: {
+			new_york_city: 8_500_00,
+			hempstead: 700_800,
+			brookhaven: 500_000
+		}
+	},
+	illinois: {
+		population:
+		cities: {
+			chicago:
+			aurora:
+			rockford:
+		}
+	}
+}
