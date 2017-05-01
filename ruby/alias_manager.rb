06d63f54e3a2989939d2ed03db11adc6caa601f3
Dan <dedinsanity@yahoo.com>
2017-04-30 20:26:43 -0500

    Add method

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -4,3 +4,30 @@ take string, split/ convert to array, isolate vowels
 advance vowels, isolate consonants, advance
 join, swap, capitalize 
 =end
+
+consonants = "bcdfghjklmnpqrstvwxyz"
+vowels = "aeiou"
+ca = consonants.split('')
+va = vowels.split('')
+new_name = []
+
+string = "Felicia Torres"
+array = string.downcase.split('')
+array.each do |x|
+  if va.index(x)  
+    nx = va[ (va.index(x).to_i) +1]
+    if nx == nil
+      nx = va[0]
+    end
+  elsif ca.index(x)
+    nx = ca[ (ca.index(x).to_i) +1]
+    if nx == nil
+      nx = ca[0]
+    end
+  else 
+    nx = x
+  end
+  new_name << nx
+end
+puts "Your original name is #{string}, 
+your new name is #{new_name.join}"
