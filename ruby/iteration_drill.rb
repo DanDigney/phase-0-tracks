3ccab032eba4758fb00fa8833c502f8be67f9722
Dan <dedinsanity@yahoo.com>
2017-05-01 06:43:13 -0500

    Add some methods

--- ruby/iteration_drill.rb
+++ ruby/iteration_drill.rb
@@ -1,5 +1,9 @@
 # Array Drills
 
+# Only managed to figure out four of these, one not even fully
+# Gonna reread & rewatch everything on what .each alone can do
+# Commented out my pseudocode 
+
 zombie_apocalypse_supplies = ["hatchet", "rations", "water jug", "binoculars",
                               "shotgun", "compass", "CB radio", "batteries"]
 
@@ -9,9 +13,9 @@ zombie_apocalypse_supplies.each {|x| print "#{x} * "}
 
 # 2. In order to keep yourself organized, sort your zombie_apocalypse_supplies
 # in alphabetical order. DO NOT USE ANY SPECIAL BUILT-IN METHODS.
-zombie_apocalypse_supplies.each do |x|
+#zombie_apocalypse_supplies.each do |x|
 
-end
+#end
 
 # 3. Create a method to see if a particular item (string) is in the
 # zombie_apocalypse_supplies. DO NOT USE ANY SPECIAL BUILT-IN METHODS.
@@ -24,11 +28,11 @@ end
 # 4. You can't carry too many things, you've only got room in your pack for 5.
 # Remove items in your zombie_apocalypse_supplies in any way you'd like,
 # leaving only 5. DO NOT USE ANY SPECIAL BUILT-IN METHODS.
-zombie_apocalypse_supplies.each do |x|
-	until zombie_apocalypse_supplies.length <= 5
-		zombie_apocalypse_supplies.delete(x)
-	end
-end
+#zombie_apocalypse_supplies.each do |x|
+#	until zombie_apocalypse_supplies.length <= 5
+#		zombie_apocalypse_supplies.delete(x)
+#	end
+#end
 
 # 5. You found another survivor! This means you can combine your supplies.
 # Create a new combined supplies list out of your zombie_apocalypse_supplies
@@ -65,9 +69,9 @@ extinct_animals.each {|x, y| print "#{x}--#{y} * "}
 # extinct 3 years before the date provided. Update the values in extinct_animals
 # so they accurately reflect what year the animal went extinct.
 # DO NOT USE ANY SPECIAL BUILT-IN METHODS.
-extinct_animals.each do |x, y|
+#extinct_animals.each do |x, y|
 	
-end
+#end
 
 
 # 4. You've heard that the following animals might be extinct, but you're not sure.
