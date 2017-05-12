4f754dd165d5c61c1f156fe45e65ec80abed7766
Dan <dedinsanity@yahoo.com>
2017-05-11 19:28:35 -0500

    Create basic loop

---
+++ js/explore.js
@@ -0,0 +1,10 @@
+// take input
+// take each character
+	// reverse them
+// print result
+
+var str = "hello"
+
+for (var counter = str.length; counter > 0; counter--) {
+	console.log(str[counter]);
+}
