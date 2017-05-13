1101c982280531dc183a0f0269d349a793a25731
unknown <dedigney@gmail.com>
2017-05-13 10:04:27 -0500

    Pseudocode solo challenge releases

---
+++ js/algos.js
@@ -0,0 +1,36 @@
+// RELEASE 0
+// grab input
+// split input
+// compare length of niputs split to eac other
+// input with longest length is its own array
+
+// RELEASE 1
+// compare the (key/value) values of objects
+// return true if ANY values are the same
+
+// RELEASE 2
+// take a number
+// number is equal to number of words in array
+// words are generated randomly
+// 	max 10.char min 1.char
+
+// DRIVER
+// call the functions of random arrays ten times
+// each time: 
+// 	generate an array 
+// 	print the array 
+// 	feed the array to the longest word array 
+// 	print the result
+// ---------------------------------------------
+
+
+function compareLength(compareable_array) {
+	x = compareable_array;
+	for(c = x.length; c > 0 ; c--) {
+		console.log(c);
+	}
+}
+
+var y = ["apple", "banana", "cashew"];
+
+compareLength(y);
